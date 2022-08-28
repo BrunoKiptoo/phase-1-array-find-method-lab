51082487b0e181f1613814c118c3a8c614c25a58
Bruno <brunochelimo@gmail.com>
2022-08-28 16:00:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(array){
+    const result = array.find(array=>(array.result ==="W"));
+    // const result = array.find(parameter(array)=> function result)
+    console.log(result);
+    if (result) {
+        return result.year;
+    } else{
+        return undefined;
+    };
+  } 
